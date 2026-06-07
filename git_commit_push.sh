7c34d0aa1af01b4bd0931a07665d867c703971fd
Jason Lillywhite <jason@lillywhite.com>
2026-06-07 11:56:30 -0700

    feat: Add native Python bindings via PyO3 and Maturin

--- git_commit_push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "Implement composite Manning's n, sediment blockage, ineffective flow area channel_area mapping, and Coriolis alpha coefficient corrections for culverts"
-git push origin solver-stabilization
